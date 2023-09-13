1d851ca71b130534f93bad01c292606f5c820301
gnuhub <gnuhub@gmail.com>
2023-09-13 17:32:24 +0800

    auto commit

--- beige-root/files/docker.install.root.sh
+++ beige-root/files/docker.install.root.sh
@@ -8,6 +8,25 @@ echo $PROJECT_NAME
 cd $CMD_PATH
 env
 
+
+
+cat /etc/passwd
+groupadd runner
+groupadd www
+useradd -m -d /home/runner -G sudo -g runner runner -s /bin/bash
+useradd -m -d /home/www -G sudo -g www www -s /bin/bash
+
+
+mkdir -p /etc/sudoers.d
+echo "www ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/root-nopassword
+echo "runner ALL=(ALL) NOPASSWD: ALL"   > /etc/sudoers.d/runner-nopassword
+chmod 750 /etc/sudoers.d/www-nopassword
+chmod 750 /etc/sudoers.d/runner-nopassword
+chmod 750 /etc/sudoers.d/
+cat /etc/passwd
+
+
+
 cd /root/
 rm -rf versions
 mkdir versions
